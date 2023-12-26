171ad9eb49721377341fd89e34bb15509d91dbfc
Scorevictim <30682499+scorevictim@users.noreply.github.com>
2023-12-25 16:33:53 -0800

    add ways of global exception handling

---
+++ BuberDinner.Api/Filters/ErrorHandlingFilterAttribute.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Net;
+
+namespace BuberDinner.Api.Filters;
+
+public class ErrorHandlingFilterAttribute : ExceptionFilterAttribute
+{
+    public override void OnException(ExceptionContext context)
+    {
+        var exception = context.Exception;
+
+        var problemDetails = new ProblemDetails
+        {
+            Type = "",
+            Title = exception.Message,
+            Status = (int)HttpStatusCode.InternalServerError,
+            
+        };
+        context.Result = new ObjectResult(problemDetails);
+        context.ExceptionHandled = true;
+    }
+}
--- BuberDinner.Api/Program.cs
+++ BuberDinner.Api/Program.cs
@@ -1,15 +1,29 @@
+using BuberDinner.Api.Filters;
+using BuberDinner.Api.Middleware;
 using BuberDinner.Application;
 using BuberDinner.Infrastructure;
+using Microsoft.AspNetCore.Diagnostics;
 
 var builder = WebApplication.CreateBuilder(args);
+{
+    builder.Services.AddApplication();
+    builder.Services.AddInfrastructure(builder.Configuration);
 
-builder.Services.AddApplication();
-builder.Services.AddInfrastructure(builder.Configuration);
-
-builder.Services.AddControllers();
+    //builder.Services.AddControllers(options => options.Filters.Add<ErrorHandlingFilterAttribute>());
+    builder.Services.AddControllers();
+}
 
 var app = builder.Build();
+{
+    //app.UseMiddleware<ErrorHandlingMiddleware>();
+    //app.UseExceptionHandler("/error");
+    app.Map("error", (HttpContext httpContext) =>
+    {
+        Exception? exception = httpContext.Features.Get<IExceptionHandlerFeature>()?.Error;
 
-app.MapControllers();
+        return Results.Problem(title: exception?.Message);
+    });
+    app.MapControllers();
+}
 
 app.Run();
